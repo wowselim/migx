00ff0ce80f30ce107b2ac831b036c532c5e92566
Selim <wowselim@live.de>
2025-04-26 11:15:18 +0300

    avoid using streams in checksum calc

--- core/src/main/java/co/selim/migx/core/impl/util/Checksums.java
+++ core/src/main/java/co/selim/migx/core/impl/util/Checksums.java
@@ -1,7 +1,9 @@
 package co.selim.migx.core.impl.util;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
-import java.util.stream.Stream;
 import java.util.zip.CRC32;
 
 public final class Checksums {
@@ -9,11 +11,18 @@ public final class Checksums {
   private Checksums() {
   }
 
-  public static int calculateChecksum(String s) {
+  public static int calculateChecksum(String input) {
     CRC32 crc32 = new CRC32();
-    try (Stream<String> lines = s.lines()) {
-      lines.forEach(line -> crc32.update(line.getBytes(StandardCharsets.UTF_8)));
+
+    try (BufferedReader reader = new BufferedReader(new StringReader(input))) {
+      String line;
+      while ((line = reader.readLine()) != null) {
+        crc32.update(line.getBytes(StandardCharsets.UTF_8));
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
+
     return (int) crc32.getValue();
   }
 }
