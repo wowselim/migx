83563f77550e38f8d2689ec73db3d294a28de43b
Selim <wowselim@live.de>
2025-04-28 00:48:59 +0300

    cleanup

--- core/src/test/java/co/selim/migx/core/MigrationTest.java
+++ core/src/test/java/co/selim/migx/core/MigrationTest.java
@@ -2,6 +2,7 @@ package co.selim.migx.core;
 
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
+import io.vertx.junit5.VertxTestContext;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,13 +17,9 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 public class MigrationTest extends IntegrationTest {
 
   @BeforeEach
-  void migrate(Vertx vertx) {
-    try {
-      getFlyway().migrate();
-      getMigx(vertx).migrate().toCompletionStage().toCompletableFuture().join();
-    } catch (Throwable t) {
-      t.printStackTrace(System.err);
-    }
+  void migrate(Vertx vertx, VertxTestContext ctx) {
+    getFlyway().migrate();
+    getMigx(vertx).migrate().onComplete(ctx.succeedingThenComplete());
   }
 
   @Test
